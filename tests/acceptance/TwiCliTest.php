ed2b1ff73cfb984a08c0c048b4059e13d0e74688
Jacopo Nardiello <jacopo.nardiello@gmail.com>
2015-02-18 17:15:25 +0000

    Heavy refactoring for the first scenario

--- tests/acceptance/TwiCliTest.php
+++ tests/acceptance/TwiCliTest.php
@@ -4,19 +4,43 @@ namespace TwiCli;
 
 class TwiCliTest extends \PHPUnit_Framework_TestCase
 {
+    public function setUp()
+    {
+        $this->twiCli = new TwiCli();
+    }
+
     public function testUserCanPostMultipleMessagesOnHisWall()
     {
-        $twiCli = new TwiCli();
-        $twiCli->process('Alice -> This is test message 1');
-        $twiCli->process('Alice -> This is test message 2');
+        $this->twiCli->process('Alice -> This is test message 1');
+        $this->twiCli->process('Alice -> This is test message 2');
 
-        $users = $twiCli->getUsers();
-        $this->assertEquals(1, count($users));
+        // We expect one user (Alice) and two messages for Alice
+        $this->assertUsersEquals(1);
+        $this->assertMessagesPerUser(2, 'Alice');
+    }
 
-        $alice = $users['Alice'];
-        $aliceWall = $alice->getMessages();
+    public function testMultipleUserCanPostOnRespectiveWall()
+    {
+        $this->twiCli->process('Alice -> This is test message');
+        $this->twiCli->process('Bob -> This is test message');
 
-        $this->assertEquals('Alice', $alice->getName());
-        $this->assertEquals(2, count($aliceWall));
+        // We expect two users with one message each
+        $this->assertUsersEquals(2);
+        $this->assertMessagesPerUser(1, 'Alice');
+        $this->assertMessagesPerUser(1, 'Bob');
+    }
+
+    private function assertUsersEquals($number)
+    {
+        $users = $this->twiCli->getUsers();
+        $this->assertEquals($number, count($users));
+    }
+
+    private function assertMessagesPerUserEquals($numMessages, $user)
+    {
+        $users = $this->twiCli->getUsers();
+        $user = $users[$user];
+        $userMessages = $user->getMessages();
+        $this->assertEquals($numMessages, count($userMessages));
     }
 }
