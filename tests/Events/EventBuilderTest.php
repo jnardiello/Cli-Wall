08a9a98fec97ee96c2614a12f5fcac9fe662ff29
Jacopo Nardiello <jacopo.nardiello@gmail.com>
2015-02-21 20:48:59 +0000

    EventBuilderTest, firstunit test for building events

---
+++ tests/Events/EventBuilderTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace TwiCli\Events;
+
+class EventBuilderTest extends \PHPUnit_Framework_TestCase
+{
+    public function testShouldBuildEvent()
+    {
+        $type = "an-event-type";
+        $origin = "an-event-origin";
+        $payload = [
+            "field-test-1" => "field-value",
+            "field-test-2" => "field-value",
+        ];
+
+        $eventBuilder = new EventBuilder();
+        $event = $eventBuilder->setType($type)
+                              ->setOrigin($origin)
+                              ->setPayload($payload);
+        $expectedEvent = new Event($type, $origin, $payload);
+
+        $this->assertEquals($expectedEvent, $event);
+    }
+}
